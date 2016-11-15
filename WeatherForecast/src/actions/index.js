189226975e9dbec34102bc2627328362a37148f7
Hein Dauven <heindauven@gmail.com>
2016-11-15 13:43:33 +0100

    Created weather fetch action: Added endpoint url and request handling.

--- WeatherForecast/src/actions/index.js
+++ WeatherForecast/src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '3db2bcaa2590bc5d10328c0105c9a5d2';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
