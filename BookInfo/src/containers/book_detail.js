a25752f0b28002f1f29e750934c505b17cc7e5e8
Hein Dauven <heindauven@gmail.com>
2016-11-15 10:34:09 +0100

    Added pages attribute to books and expanded book details to display the nr. of pages.

--- BookInfo/src/containers/book_detail.js
+++ BookInfo/src/containers/book_detail.js
@@ -3,8 +3,16 @@ import { connect } from 'react-redux';
 
 class BookDetail extends Component {
   render() {
+    if (!this.props.book) {
+      return <div>Select a book to get started.</div>;
+    }
+
     return (
-      <div>Book Detail</div>
+      <div className="card-block">
+        <h3 className="card-title">Details for:</h3>
+        <div className="card-text">Title: {this.props.book.title}</div>
+        <div className="card-text">Pages: {this.props.book.pages}</div>
+      </div>
     );
   }
 }
--- BookInfo/src/reducers/reducer_books.js
+++ BookInfo/src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function() {
   return [
-    { title: 'JavaScript: The Good Parts'},
-    { title: 'Harry Potter'},
-    { title: 'The Dark Tower'},
-    { title: 'Eloquent Ruby'},
-    { title: 'Elixir in Action'}
+    { title: 'JavaScript: The Good Parts', pages: 101},
+    { title: 'Harry Potter', pages: 307},
+    { title: 'The Dark Tower', pages: 294},
+    { title: 'Eloquent Ruby', pages: 573},
+    { title: 'Elixir in Action', pages: 826}
   ];
 }
